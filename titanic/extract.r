3081614c3e3893d09892e870d21a93a6b640fbee
unknown <marcos@tiendanube.com>
2018-05-22 19:12:15 -0300

    Funcionando

--- titanic/extract.r
+++ titanic/extract.r
@@ -19,100 +19,34 @@ print('Load training data')
 train_set <- read.csv(paste(data_dir,'/train.csv',sep=''), header = TRUE) #30% of data to fit the model
 eval_set <- read.csv(paste(data_dir,'/test.csv',sep=''), header = TRUE) #30% of data to fit the model
 str(train_set)
+str(eval_set)
 
-#create a traingin and a validation set
-print('Prepare training data')
-val_index <- sample(c(1:nrow(data_set)), nrow(data_set)*.1) # 90% para training - 10% Evaluacion
-data_set$train_sample <- "training"
-data_set[val_index, "train_sample"] <- "validation"  # Se deja septiembre para validación
-data_set$Label <- ifelse(data_set$Label, "click", "no_click")
-data_set$id <- NA # Needed to merge with eval_set
+train_set$Survived <- as.factor(ifelse(train_set$Survived,'alive','dead'))
+eval_set$Survived <- NA
 
-print('Load evaluation data')
-eval_set <- load_csv_data(paste(data_dir,'/ctr_test.csv', sep = ''))
-print('Prepare evaluation data')
-eval_set$train_sample <- "evaluation" #Needed to merge with data_set
-eval_set$Label <- NA #Needed to merge with data_set
-
-print('Join data sets')
-data_set <- rbind(data_set,eval_set) #merge training, validation and evaluation data sets
-rm(eval_set)
-
-print('Create temp training_set')
-training_set <- data_set[data_set$train_sample =="training",]
-#data_set <- readRDS(paste(data_dir,'/data_set_mini.rds', sep = ''))
-
+data_set <- rbind(train_set,eval_set)
 str(data_set)
 
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 # FEATURE ENGINEERING
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 print('FEATURE ENGINEERING')
 
-data_set$Label <- as.factor(data_set$Label)
-data_set$id <- as.factor(data_set$id)
-data_set$train_sample <- as.factor(data_set$train_sample)
+data_set$cabin_letter <- as.factor(substr(data_set$Cabin,0,1))
+data_set[data_set$cabin_letter == '','cabin_letter'] <- NA
 
-print('Procesing day & hour')
-data_set$hour <- (as.integer(format(as.POSIXct(data_set$auction_time, origin="1970-01-01"), "%H")) + ceiling(data_set$timezone_offset)) %% 24
-data_set$day <- as.integer(format(as.POSIXct(data_set$auction_time + data_set$timezone_offset * 3600, origin="1970-01-01"), "%w"))
+data_set[data_set$Embarked == '','Embarked'] <- NA
+data_set$Pclass <- as.factor(data_set$Pclass) 
+data_set$family_size <- data_set$SibSp + data_set$Parch
 
-print('Procesing Creative Size')
-data_set$creative_size <- data_set$creative_height * data_set$creative_width
+data_set$title <- gsub('(.*, )|(\\..*)', '', data_set$Name)
+data_set[!(data_set$title %in% c('Master','Miss','Mr','Mrs')),'title'] <- 'Other'
+data_set$title <- as.factor(data_set$title)
 
-print('Converting categorical variables to numeric')
-# data_set$action_categorical_0   <- as.numeric(data_set$action_categorical_0   )
-# data_set$action_categorical_1   <- as.numeric(data_set$action_categorical_1   )
-data_set$action_categorical_2   <- as.numeric(data_set$action_categorical_2   )
-# data_set$action_categorical_3   <- as.numeric(data_set$action_categorical_3   )
-data_set$action_categorical_4   <- as.numeric(data_set$action_categorical_4   )
-data_set$action_categorical_5   <- as.numeric(data_set$action_categorical_5   )
-data_set$action_categorical_6   <- as.numeric(data_set$action_categorical_6   )
-data_set$action_categorical_7   <- as.numeric(data_set$action_categorical_7   )
-#data_set$action_list_0          <- as.numeric(data_set$action_list_0          )
-data_set$action_list_1          <- as.numeric(data_set$action_list_1          )
-data_set$action_list_2          <- as.numeric(data_set$action_list_2          )
-# data_set$auction_boolean_0      <- as.numeric(data_set$auction_boolean_0      )
-# data_set$auction_boolean_1      <- as.numeric(data_set$auction_boolean_1      )
-# data_set$auction_boolean_2      <- as.numeric(data_set$auction_boolean_2      )
-data_set$auction_categorical_0  <- as.numeric(data_set$auction_categorical_0  )
-# data_set$auction_categorical_1  <- as.numeric(data_set$auction_categorical_1  )
-# data_set$auction_categorical_10 <- as.numeric(data_set$auction_categorical_10 )
-data_set$auction_categorical_11 <- as.numeric(data_set$auction_categorical_11 )
-data_set$auction_categorical_12 <- as.numeric(data_set$auction_categorical_12 )
-# data_set$auction_categorical_2  <- as.numeric(data_set$auction_categorical_2  )
-data_set$auction_categorical_3  <- as.numeric(data_set$auction_categorical_3  )
-# data_set$auction_categorical_4  <- as.numeric(data_set$auction_categorical_4  )
-# data_set$auction_categorical_5  <- as.numeric(data_set$auction_categorical_5  )
-data_set$auction_categorical_6  <- as.numeric(data_set$auction_categorical_6  )
-data_set$auction_categorical_7  <- as.numeric(data_set$auction_categorical_7  )
-# data_set$auction_categorical_8  <- as.numeric(data_set$auction_categorical_8  )
-data_set$auction_categorical_9  <- as.numeric(data_set$auction_categorical_9  )
-data_set$auction_list_0         <- as.numeric(data_set$auction_list_0         )
-data_set$creative_categorical_0 <- as.numeric(data_set$creative_categorical_0 )
-# data_set$creative_categorical_1 <- as.numeric(data_set$creative_categorical_1 )
-# data_set$creative_categorical_10<- as.numeric(data_set$creative_categorical_10)
-# data_set$creative_categorical_11<- as.numeric(data_set$creative_categorical_11)
-# data_set$creative_categorical_12<- as.numeric(data_set$creative_categorical_12)
-data_set$creative_categorical_2 <- as.numeric(data_set$creative_categorical_2 )
-# data_set$creative_categorical_3 <- as.numeric(data_set$creative_categorical_3 )
-# data_set$creative_categorical_4 <- as.numeric(data_set$creative_categorical_4 )
-# data_set$creative_categorical_5 <- as.numeric(data_set$creative_categorical_5 )
-data_set$creative_categorical_6 <- as.numeric(data_set$creative_categorical_6 )
-# data_set$creative_categorical_7 <- as.numeric(data_set$creative_categorical_7 )
-# data_set$creative_categorical_8 <- as.numeric(data_set$creative_categorical_8 )
-# data_set$creative_categorical_9 <- as.numeric(data_set$creative_categorical_9 )
-# data_set$gender                 <- as.numeric(data_set$gender                 )
-# data_set$has_video              <- as.numeric(data_set$has_video              )
-
-print('Removing unwanted variables')
-data_set <- select(data_set,-auction_time,-timezone_offset,-creative_width,-creative_height,-action_categorical_5,
-                     -action_categorical_6,-action_categorical_7,-auction_age,-auction_boolean_1,-auction_boolean_2,
-                      -auction_categorical_10,-auction_categorical_2,-auction_categorical_4,-auction_list_0,
-                      -creative_categorical_1,-creative_categorical_10,-creative_categorical_3,
-                      -creative_categorical_5,-creative_categorical_8,-device_id,-device_id_type)
-
-str(data_set)
-
+print('drop unussed cols')
+data_set <- drop_cols_(data_set,c('Name','Cabin','Ticket','SibSp','Parch'))
 print('saving Data Set')
-saveRDS(data_set, file = paste(data_dir,'/data_set_',data_percentage_to_load,'_v3.6.1.rds', sep = ''))
+
+train_set <- data_set[!is.na(data_set$Survived),]
+eval_set <- data_set[is.na(data_set$Survived),]
+
+saveRDS(train_set, file = paste(data_dir,'/train_set.rds', sep = ''))
+saveRDS(eval_set, file = paste(data_dir,'/eval_set.rds', sep = ''))
--- titanic/fit.r
+++ titanic/fit.r
@@ -1,98 +1,79 @@
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 # SETUP
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 #Clean memmory
 rm(list = ls())
 
 #Define working dirs
-data_dir <- 'C:/Users/Marcos/OneDrive - Tienda Nube/MiM/Mineria de Datos/tp/data'
-script_dir <- 'C:/Users/Marcos/OneDrive - Tienda Nube/MiM/code/MiM/mineria_de_datos/best_result_2'
-
-# script_dir <- '/mnt/c/Users/Marcos/OneDrive - Tienda Nube/MiM/code/MiM/mineria_de_datos/best_result_2'
-# data_dir <- '/mnt/c/Users/Marcos/OneDrive - Tienda Nube/MiM/Mineria de Datos/tp/data'
+data_dir <- 'C:/Users/Marcos/Documents/Kaggle/titanic'
 #Load required libraries
-source(paste(script_dir,'/utils.R', sep = '')) #My custom made functions
-library('caret')
-library('dplyr')
-
-
-memory.limit(50000) #Incremento la memoria para que no de error
-#Parece que es mas lento.
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+library(caret)
+library(dplyr)
+library(MLmetrics)
 # DATA LOAD (build working data_set)
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 print('DATA LOAD')
+train_set <- readRDS(paste(data_dir,'/train_set.rds', sep = ''))
+str(train_set)
 
-#data_set <- readRDS(paste(data_dir,'/data_set_full.rds', sep = ''))
-data_set <- readRDS(paste(data_dir,'/data_set_0.4_v3.6.1.rds', sep = ''))
-str(data_set)
-
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 # MODEL FIT
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 print('MODEL FIT')
-
-# Creo los indices de validacion y entrenamiento
-# indexIn <- list()
-# indexIn[["tr1"]] <- which(data_set$train_sample=="training")  # Indico con qué entrenar
-# indexOut <- list()
-# indexOut[["tr1"]] <- which(data_set$train_sample=="validation")  # Indico con qué validar
-#
 #CREO EL MODELO
 fitControl <- trainControl(method="LGOCV",  # Esto da igual
-                           number = 5,
-                           # repeats = 3,
-                           # index=indexIn, #Voy a entrenar con estas filas
-                           # indexOut=indexOut, #voy a validar con estas filas
+                           number = 3,
                            verboseIter=TRUE,
                            returnData=FALSE, # No le pido que me devuelve el dataset OJO! dataset muy grandes
                            summaryFunction=twoClassSummary, #Te devuelve el area bajo la curva de ROC
                            classProbs=TRUE)
 
+xgbGrid <- expand.grid(nrounds = c(100,150),
+                      max_depth = c(1,2,5),
+                      eta = c(0.2, 0.3, 0.4),
+                      gamma = c(0,1,3,5,10),
+                      colsample_bytree = seq(0.5, 1, by = 0.05),
+                      min_child_weight = c(1, 2, 5,10),
+                      subsample = seq(0.5, 1, by = 0.05))
 #Armo todas las combinaciones posibles de hiperparametros -> Grilla.
-xgbGrid <- expand.grid(nrounds = c(100),
-                       max_depth = c(10),
-                       eta = c(0.2),
-                       gamma = c(1),
-                       colsample_bytree = c(0.75),
-                       min_child_weight = c(10),
-                       subsample = c(0.95))
+# xgbGrid <- expand.grid(nrounds = c(100),
+#                        max_depth = c(10),
+#                        eta = c(0.2),
+#                        gamma = c(1),
+#                        colsample_bytree = c(0.75),
+#                        min_child_weight = c(10),
+#                        subsample = c(0.95))
 
 # Entreno el modelo con una seleccion de 15 combinaciones de la grilla
-xgbFit <- train(Label ~ ., data=select(data_set[data_set$train_sample!="evaluation",],-id,-train_sample),
+xgbFit <- train(Survived ~ ., data=select(train_set,-PassengerId),
                 method = "xgbTree",
                 trControl = fitControl,
-                tuneGrid = xgbGrid, #Sampleo 15 puntos de la grilla de hiperparametros
+                tuneGrid = xgbGrid[sample(c(1:nrow(xgbGrid)), 60),], #Sampleo 15 puntos de la grilla de hiperparametros
                 metric = "ROC",
                 na.action = na.pass,
                 allowParallel=TRUE)
 
-print('BEST FITTING RESULT')
-print(xgbFit$results[which.max(xgbFit$results$ROC),]) #Imprimo el ROC optimo del modelo.
-write.table(varImp(xgbFit)$importance,paste(data_dir,'/var_importance_model_',format(Sys.time(), "%Y%m%d%H%M%S"),'.csv', sep = ''),
-           sep=',', quote=FALSE)
-write.table(xgbFit$results[which.max(xgbFit$results$ROC),],paste(data_dir,'/best_auc_model_',format(Sys.time(), "%Y%m%d%H%M%S"),'.csv', sep = ''),
-           sep=',', quote=FALSE, row.names=FALSE)
-saveRDS(xgbFit, file = paste(data_dir,'/model_',format(Sys.time(), "%Y%m%d%H%M%S"),'.rds', sep = ''))
-# png(paste(data_dir,'/model_',format(Sys.time(), "%Y%m%d%H%M%S"),'.png', sep = ''))
-# plot(varImp(xgbFit)) #Veo la importancia de las variables
-# dev.off()
+# print('BEST FITTING RESULT')
+# print('eta: %f | max_depth: %i | colsample_bytree: %f | min_child_weight: %i | subsample: %f | nrounds: %i | gamma: %i',
+#           xgbFit$results[which.max(xgbFit$results$ROC),'eta'],xgbFit$results[which.max(xgbFit$results$ROC),'max_depth'],
+#           xgbFit$results[which.max(xgbFit$results$ROC),'colsample_bytree'],xgbFit$results[which.max(xgbFit$results$ROC),'min_child_weight'],
+#           xgbFit$results[which.max(xgbFit$results$ROC),'subsample'],xgbFit$results[which.max(xgbFit$results$ROC),'nrounds'],
+#           xgbFit$results[which.max(xgbFit$results$ROC),'gamma'])
+# print('ROC: %f ',xgbFit$results[which.max(xgbFit$results$ROC),'ROC'])
+print(xgbFit$results[which.max(xgbFit$results$ROC),'ROC'])
+print(varImp(xgbFit)$importance,row.names = TRUE)
 
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 # PREDICT
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 print('PREDICT')
+eval_set <- readRDS(paste(data_dir,'/eval_set.rds', sep = ''))
 # Hago la prediccion con los datos de evaluacion
 preds_eval <- predict(xgbFit,
-                      newdata = select(data_set[data_set$train_sample=="evaluation",],-id,-train_sample),
-                      type="prob", #Devolveme las probabilidades
+                      newdata = select(eval_set,-PassengerId),
+                      type="raw", #Devolveme las probabilidades
                       na.action = na.pass)
 
 # Lo paso a un archivo de texto
 options(scipen=10) #para sacar la notacion cientifica
 #Creo el data set para hacer submit en kaggle (id, prob)
-submit <- data.frame(id=data_set[data_set$train_sample=="evaluation","id"],
-                    pred=preds_eval$click)
+submit <- data.frame(PassengerId=eval_set$PassengerId,
+                    Survived= ifelse(preds_eval=='dead',0,1))
+
+# F1_Score(y_pred = preds_eval, y_true = eval_set$Survived, positive = 'alive')
 #Exporto a csv el dataset para subir a kaggle
 write.table(submit,
             paste(data_dir,'/preds_submit_',format(Sys.time(), "%Y%m%d%H%M%S"),'.csv', sep = ''),
